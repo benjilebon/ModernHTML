e6df71ddd09ee9b9395ca4cbcfafe5751f918ac3
Edouard Courty <edouard.courty2@gmail.com>
2018-12-19 14:45:14 +0100

    Refactored code, better indentation.

--- core.js
+++ core.js
@@ -1,84 +1,79 @@
 const istype = {
-    arr: a => Array.isArray(a),
-    obj: a => stringContains(Object.prototype.toString.call(a), 'Object'),
-    str: a => typeof a === 'string',
-    fnc: a => typeof a === 'function',
-    und: a => typeof a === 'undefined',
-    htc: a => HTMLCollection.prototype.isPrototypeOf(a)
+  arr: a => Array.isArray(a),
+  obj: a => stringContains(Object.prototype.toString.call(a), 'Object'),
+  str: a => typeof a === 'string',
+  fnc: a => typeof a === 'function',
+  und: a => typeof a === 'undefined',
+  htc: a => HTMLCollection.prototype.isPrototypeOf(a)
 }
 
 function ModernHTML(){
 
-    function createTag(e){
-        if (!istype.str(e)) return;
-        tag = document.getElementsByTagName(e);
-        attr = [];   
-        
-        function replaceTag(f, elem){
-            if (istype.htc(elem)) {
-                for (i=elem.length-1; i > -1; i--) {
-                    r = document.createElement(f);
+  function createTag(e){
+    if (!istype.str(e)) return
+    tag = document.getElementsByTagName(e)
+    attr = []
+    
+    function replaceTag(f, elem){
+      if (istype.htc(elem)) {
+        for (i=elem.length-1; i > -1; i--) {
+          r = document.createElement(f);
 
-                    for (index = elem[i].attributes.length - 1; index >= 0; --index) {
-                        r.attributes.setNamedItem(elem[i].attributes[index].cloneNode());
-                    }
-                    h = elem[i].parentNode.replaceChild(r, elem[i]);
-                }                
-            } else {
-                r = document.createElement(f);
+          for (index = elem[i].attributes.length - 1; index >= 0; --index) {
+            r.attributes.setNamedItem(elem[i].attributes[index].cloneNode())
+          }
+          h = elem[i].parentNode.replaceChild(r, elem[i])
+        }                
+      } else {
+        r = document.createElement(f);
 
-                for (index = elem.attributes.length - 1; index >= 0; --index) {
-                    r.attributes.setNamedItem(elem.attributes[index].cloneNode());
-                }
-                elem.parentNode.replaceChild(r, elem);
-
-                return r;
-            }
+        for (index = elem.attributes.length - 1; index >= 0; --index) {
+          r.attributes.setNamedItem(elem.attributes[index].cloneNode())
         }
-        
-        function createAttr(name, callback){
-            if (!(istype.str(name) && istype.fnc(callback))) return;
-            if (istype.str(name)) {
-                attr.push(name);
-                for (i=tag.length-1; i > -1; i--) {
-                    if (tag[i].hasAttribute(name)) {
-                        callback(tag[i]);
-                    }
-                }
-            } else if (istype.htc(name)) {
-                callback(name);
-            }
-
+        elem.parentNode.replaceChild(r, elem)
+        return r
+      }
+    }
+    
+    function createAttr(name, callback){
+      if (!(istype.str(name) && istype.fnc(callback))) return
+      if (istype.str(name)) {
+        attr.push(name)
+        for (i=tag.length-1; i > -1; i--) {
+          if (tag[i].hasAttribute(name)) {
+            callback(tag[i])
+          }
         }
+      } else if (istype.htc(name)) {
+        callback(name)
+      }
+    }
 
-        function defaultAttr(a) {
-            if (!istype.arr(a)) return;
-            for (i=tag.length-1; i > -1; i--) { //check pour toute les itérations de balise tag
-                if (tag[i].attributes.length === 0){
-                    setAttr(tag[i], a);
-                }
-            }
+    function defaultAttr(a) {
+      if (!istype.arr(a)) return;
+      for (i=tag.length-1; i > -1; i--) { //check pour toute les itérations de balise tag
+        if (tag[i].attributes.length === 0){
+          setAttr(tag[i], a)
         }
+      }
+    }
 
-        function setAttr(elem, a) {
-            for (i=0; i<a.length;i++) //boucle pour tout les attributs par défaut
-                elem.setAttribute(a, "");
-        }
-
-        return {
-            createAttr : createAttr,
-            replaceTag : replaceTag,
-            defaultAttr : defaultAttr,
-            value : tag
-        };
+    function setAttr(elem, a) {
+      for (i=0; i<a.length;i++) //boucle pour tout les attributs par défaut
+        elem.setAttribute(a, "")
     }
 
     return {
-        createTag : createTag
-    };
+      createAttr : createAttr,
+      replaceTag : replaceTag,
+      defaultAttr : defaultAttr,
+      value : tag
+    }
+  }
 
+  return {
+    createTag : createTag
+  }
 }
-
-
 
 /* ------------HTML TEST CODE--------------------*/
