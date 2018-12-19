1aea8e945106ab1bf863c8e0ca001bbfb8353494
Edouard Courty <edouard.courty2@gmail.com>
2018-12-19 14:53:10 +0100

    Refactored code (spaces, var standards)

--- core.js
+++ core.js
@@ -7,25 +7,25 @@ const istype = {
   htc: a => HTMLCollection.prototype.isPrototypeOf(a)
 }
 
-function ModernHTML(){
+function ModernHTML () {
 
-  function createTag(e){
+  function createTag (e) {
     if (!istype.str(e)) return
     tag = document.getElementsByTagName(e)
     attr = []
-    
-    function replaceTag(f, elem){
-      if (istype.htc(elem)) {
-        for (i=elem.length-1; i > -1; i--) {
-          r = document.createElement(f);
 
-          for (index = elem[i].attributes.length - 1; index >= 0; --index) {
+    function replaceTag (f, elem) {
+      if (istype.htc(elem)) {
+        for (var i = elem.length - 1; i > -1; i--) {
+          r = document.createElement(f)
+
+          for (var index = elem[i].attributes.length - 1; index >= 0; --index) {
             r.attributes.setNamedItem(elem[i].attributes[index].cloneNode())
           }
           h = elem[i].parentNode.replaceChild(r, elem[i])
-        }                
+        }
       } else {
-        r = document.createElement(f);
+        r = document.createElement(f)
 
         for (index = elem.attributes.length - 1; index >= 0; --index) {
           r.attributes.setNamedItem(elem.attributes[index].cloneNode())
@@ -35,11 +35,11 @@ function ModernHTML(){
       }
     }
     
-    function createAttr(name, callback){
+    function createAttr (name, callback) {
       if (!(istype.str(name) && istype.fnc(callback))) return
       if (istype.str(name)) {
         attr.push(name)
-        for (i=tag.length-1; i > -1; i--) {
+        for (var i = tag.length - 1; i > -1; i--) {
           if (tag[i].hasAttribute(name)) {
             callback(tag[i])
           }
@@ -49,31 +49,32 @@ function ModernHTML(){
       }
     }
 
-    function defaultAttr(a) {
-      if (!istype.arr(a)) return;
-      for (i=tag.length-1; i > -1; i--) { //check pour toute les itérations de balise tag
-        if (tag[i].attributes.length === 0){
+    function defaultAttr (a) {
+      if (!istype.arr(a)) return
+      for (var i = tag.length - 1; i > -1; i--) { // Check pour toute les itérations de balise tag
+        if (tag[i].attributes.length === 0) {
           setAttr(tag[i], a)
         }
       }
     }
 
-    function setAttr(elem, a) {
-      for (i=0; i<a.length;i++) //boucle pour tout les attributs par défaut
-        elem.setAttribute(a, "")
+    function setAttr (elem, a) {
+      for (var i = 0; i < a.length; i++) { // Boucle pour tout les attributs par défaut
+        elem.setAttribute(a, '')
+      }
     }
 
     return {
-      createAttr : createAttr,
-      replaceTag : replaceTag,
-      defaultAttr : defaultAttr,
-      value : tag
+      createAttr: createAttr,
+      replaceTag: replaceTag,
+      defaultAttr: defaultAttr,
+      value: tag
     }
   }
 
   return {
-    createTag : createTag
+    createTag: createTag
   }
 }
 
-/* ------------HTML TEST CODE--------------------*/
+/* ------------ HTML TEST CODE -------------------- */
